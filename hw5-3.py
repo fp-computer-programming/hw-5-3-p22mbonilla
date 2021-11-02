526013eabdf0b7a7a199aa60d28443bdb32bb412
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-02 09:52:06 -0400

    add file

---
+++ hw5-3.py
@@ -0,0 +1,17 @@
+# Author MB 11/02/2021
+
+import calendar
+
+# Question 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+print(calendar.firstweekday("Su"))
+
+# Question 3
+print(calendar.monthcalendar(2004, 12))
+
+# Question 5
+print(calendar.isleap)
+
+#  ^returns <function isleap at 0x7fc0eb223dc0>
