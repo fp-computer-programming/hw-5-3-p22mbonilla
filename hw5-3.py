bfbf04d6433bdd47d5c9449a6b16d0509e7d3b52
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-02 10:00:33 -0400

    done

--- hw5-3.py
+++ hw5-3.py
@@ -3,15 +3,15 @@
 import calendar
 
 # Question 1
-calendar.TextCalendar().pryear(2020)
+# calendar.TextCalendar().pryear(2020)
 
 # Question 2
-calendar.setfirstweekday
+# calendar.setfirstweekday(calendar.SUNDAY)
 
 # Question 3
-calendar.monthcalendar(2004, 12)
+# calendar.monthcalendar(2004, 12)
 
 # Question 5
-print(calendar.isleap)
+# print(calendar.isleap)
 
 #  ^returns <function isleap at 0x7fc0eb223dc0>
